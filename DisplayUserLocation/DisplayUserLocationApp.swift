e6aac8a2aa4bdbd9b7fef90944283439c3e5f1f0
Ismay1l <ismayilovismayil1226@gmail.com>
2024-05-18 21:07:28 -0400

    Initial Commit

---
+++ DisplayUserLocation/DisplayUserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayUserLocationApp.swift
+//  DisplayUserLocation
+//
+//  Created by Ismayil Ismayilov on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct DisplayUserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
